356d5d157f1b887b54b6f04f937d554e35b25f36
Nakisha Fouch <dnb7rz@virginia.edu>
2024-08-26 09:54:34 -0400

    rm positinn option.

--- Distros_GGPlot.R
+++ Distros_GGPlot.R
@@ -4,14 +4,14 @@ set.seed(1234)
 Nums = data.frame(vals = rnorm(200))
 
 ggplot(Nums, aes(x=vals)) +
-  geom_histogram(binwidth=.1, alpha=.5, position="identity")
+  geom_histogram(binwidth=.1, alpha=.5)
 
 BiNums = data.frame(vals = rbinom(n=100, size=1, prob=0.5))
 
 ggplot(BiNums, aes(x=vals)) +
-  geom_histogram(binwidth=.1, alpha=.5, position="identity")
+  geom_histogram(binwidth=.1, alpha=.5)
 
-PoiNum = data.frame(vals = rpois(n = 100, lambda = 0.5))
+PoiNums = data.frame(vals = rpois(n = 100, lambda = 0.5))
 
 ggplot(PoiNums, aes(x=vals)) +
-  geom_histogram(binwidth=.1, alpha=.5, position="identity")
+  geom_histogram(binwidth=.1, alpha=.5)
