88d70350f0fa0bb2f46952e5f3565f4feb6e6781
Nakisha Fouch <dnb7rz@virginia.edu>
2024-08-26 09:55:51 -0400

    rm poistion option and made bars larger.

--- CLT_GGPlot.R
+++ CLT_GGPlot.R
@@ -6,10 +6,10 @@ set.seed(1234)
 BiNums = data.frame(vals = rbinom(n=500, size=30, prob=0.5))
 
 ggplot(BiNums, aes(x=vals)) +
-  geom_histogram(binwidth=.2, alpha=.5, position="identity")
+  geom_histogram(binwidth=.5, alpha=.5)
 
 ggplot(BiNums, aes(x=vals)) + 
   geom_histogram(aes(y=after_stat(density)),      
-                 binwidth=.2) +
+                 binwidth=.5) +
   geom_density(alpha=.2, fill="#FF6666")  
 
